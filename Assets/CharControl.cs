f4084a874c9ebec1c92710c9989f3269e65452df
king kule <chortletortle@gmail.com>
2017-07-13 00:39:23 -0400

    gave up on trying to add gravity, collisions arent working, better camera movement

--- Assets/CharControl.cs
+++ Assets/CharControl.cs
@@ -13,9 +13,18 @@ public class CharControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float horizontal = Input.GetAxis ("Horizontal"); // A+D on keyboard
+		//float horizontal = Input.GetAxis ("Horizontal"); // A+D on keyboard
 		float vertical = Input.GetAxis ("Vertical");// W+S on keyboard
+		transform.Translate(new Vector3(0f, 0f, vertical) * .5f /*+ Physics.gravity *0.01f this causes collision issues*/);
+		if (Input.GetKey(KeyCode.A)){
+			cc.transform.Rotate(0f, -1f, 0f); //rotate left on the Y axis
+		}
+		if (Input.GetKey(KeyCode.D)){
+			cc.transform.Rotate(0f, 1f, 0f); //rotate left on the Y axis
+		}
+
+	
 		//apply our input to our character controller
-		cc.Move( new Vector3(horizontal, 0f, vertical) * .5f + Physics.gravity *0.01f);
+		//cc.Move( new Vector3(0f, 0f, vertical) * .5f + Physics.gravity *0.01f);
 	}
 }

18bd9ed27a39ae7575c9f18f8e413ade4e0a7d34
king kule <chortletortle@gmail.com>
2017-07-13 01:33:52 -0400

    converted translate to move in order for collisions to work, but also fixed rotation using TransformDirection

--- Assets/CharControl.cs
+++ Assets/CharControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CharControl : MonoBehaviour {
 
 	CharacterController cc;
+	public float speed = .5F;
+	public float rotationSpeed = 15.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -14,14 +16,13 @@ public class CharControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//float horizontal = Input.GetAxis ("Horizontal"); // A+D on keyboard
-		float vertical = Input.GetAxis ("Vertical");// W+S on keyboard
-		transform.Translate(new Vector3(0f, 0f, vertical) * .5f /*+ Physics.gravity *0.01f this causes collision issues*/);
-		if (Input.GetKey(KeyCode.A)){
-			cc.transform.Rotate(0f, -1f, 0f); //rotate left on the Y axis
-		}
-		if (Input.GetKey(KeyCode.D)){
-			cc.transform.Rotate(0f, 1f, 0f); //rotate left on the Y axis
-		}
+		float vertical = Input.GetAxis ("Vertical") * speed;// W+S on keyboard
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+		transform.Rotate(0, rotation, 0);
+		//cc.transform.Rotate(0, rotation, 0);
+		cc.Move(transform.TransformDirection(Vector3.forward*vertical) + Physics.gravity *0.01f);
+
 
 	
 		//apply our input to our character controller

4770d9e93b914c3889f47ee0fd995503cb2dc860
king kule <chortletortle@gmail.com>
2017-07-13 04:25:37 -0400

    attempted to make sky box wonderful kittens but gave up and successfully finished implementing text

--- Assets/CharControl.cs
+++ Assets/CharControl.cs
@@ -9,7 +9,14 @@ public class CharControl : MonoBehaviour {
 	public float speed = 1F;
 	public float rotationSpeed = 15.0F;
 	public Text screenMessage;
+	public Text title;
 	bool startText = true;
+	public GameObject fridge;
+	public GameObject oven;
+	public GameObject cup;
+	public GameObject bed;
+	public GameObject vent;
+	int textCount=0;
 
 	// Use this for initialization
 	void Start () {
@@ -19,9 +26,46 @@ public class CharControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (startText == true && (Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0)) {
-			screenMessage.text = "";
+			screenMessage.text = "WAIT!!! \n FOOD!!!! \n KITCHEN!!!";
+			textCount++;
+			title.text = "";
 			startText = false;
 		}
+
+		if (Vector3.Distance (transform.position, fridge.transform.position) < 10f && textCount == 1) { 
+			screenMessage.fontSize = 30;
+			screenMessage.text = "fate reminds you \nthat cats don't have thumbs.\n you feel hot \n but without your bikini on! \n why?";
+			textCount++;
+		} 
+
+		if ( Vector3.Distance(transform.position, oven.transform.position) < 7f && textCount == 2) { 
+			screenMessage.fontSize = 25;
+			screenMessage.text = "this must be Steve Buscemi \n because its \n TOO HOT! \n something behind you \n fills you with warmth";
+			textCount++;
+		}
+
+		if ( Vector3.Distance(transform.position, cup.transform.position) < 5f && textCount == 3) { 
+			screenMessage.fontSize = 30;
+			screenMessage.text = "the food guy \n wrote you \n a kind letter \n and a fat check \n go sit on his face";
+			textCount++;
+		}
+
+		if ( Vector3.Distance(transform.position, bed.transform.position) < 4f && textCount == 4) { 
+			screenMessage.fontSize = 30;
+			screenMessage.text = "HEY! \n wtf!!! \n food guy aint here \n you hear a noise \n and yawn";
+			textCount++;
+		} 
+
+		if ( Vector3.Distance(transform.position, vent.transform.position) < 3f && textCount == 5) { 
+			textCount++;
+			screenMessage.fontSize = 30;
+			screenMessage.text = "the wafting temperature \n hits the spot \n on your fuzzy balls \n\n hit space to snooze";
+
+		}
+
+
+
+
 		//float horizontal = Input.GetAxis ("Horizontal"); // A+D on keyboard
 		float vertical = Input.GetAxis ("Vertical") * speed;// W+S on keyboard
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
